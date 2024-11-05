822ce36dd9176e73bfef68f83d4f538bc8cf1f45
Hikanyan <hikanyan0131@gmail.com>
2024-11-06 01:12:24 +0900

    Update ;

--- Assets/HikanyanLaboratory/Script/CriAddressable/CriAddressableAudioManagerTests.cs
+++ Assets/HikanyanLaboratory/Script/CriAddressable/CriAddressableAudioManagerTests.cs
@@ -7,14 +7,33 @@ using UnityEngine.TestTools;
 
 public class CriAddressableAudioManagerTests : MonoBehaviour
 {
-    
     [SerializeField] CueReference cueReference;
+    [SerializeField, Range(0f, 1f)] private float value = 0.5f; 
     private CriAddressableAudioManager _audioManager;
+    private CriAddressableAudioManager.SimplePlayback _playback;
 
-    async void Start()
+    private async void Start()
     {
         _audioManager = CriAddressableAudioManager.Instance;
-        await _audioManager.StartPlayback(cueReference);
+
+        // 再生を開始し、完了を待機
+        _playback = await _audioManager.StartPlayback(cueReference);
     }
-    
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // 再生中の場合、ボリュームとピッチを変更
+            if (_playback.IsPlaying())
+            {
+                _playback.SetVolumeAndPitch(value, -0.5f); // ボリュームを0.5、ピッチを-0.5に設定
+                Debug.Log("Volume and pitch adjusted to 0.5 and -0.5.");
+            }
+            else
+            {
+                Debug.LogWarning("Playback is not active.");
+            }
+        }
+    }
 }
