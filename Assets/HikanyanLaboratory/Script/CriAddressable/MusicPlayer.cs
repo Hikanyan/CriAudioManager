21c2916313501456db43d237fac5e01ee9f83d49
Hikanyan <hikanyan0131@gmail.com>
2024-11-10 20:38:25 +0900

    Update :  Dropdown

--- Assets/HikanyanLaboratory/Script/CriAddressable/MusicPlayer.cs
+++ Assets/HikanyanLaboratory/Script/CriAddressable/MusicPlayer.cs
@@ -5,46 +5,60 @@ using R3;
 
 namespace HikanyanLaboratory
 {
-    // 曲のリストのオブジェクトは軽量化のため使い回しをする
-
-
     public class MusicPlayer : MonoBehaviour
     {
-        [SerializeField] private Button _PlayButton;
+        [SerializeField] private Button _playButton;
         [SerializeField] private Button _nextButton;
-        [SerializeField] private Button _BackButton;
-        [SerializeField] private Slider _VolumeSlider;
+        [SerializeField] private Button _backButton;
+        [SerializeField] private Slider _volumeSlider;
         [SerializeField] private Slider _progressSlider;
-        [SerializeField] private Dropdown _MusicDropdown;
+        [SerializeField] private Dropdown _musicDropdown;
 
         [SerializeField] private Text _musicNameText;
         [SerializeField] private List<CueReference> _audioList = new List<CueReference>();
 
         private CriAddressableAudioManager _criAddressableAudioManager;
-        private CriAddressableAudioManager.SimplePlayback _currentPlayback;
+        private CriAddressableAudioManager.SimplePlayback? _currentPlayback;
 
         public ReactiveProperty<int> CurrentIndex { get; private set; } = new ReactiveProperty<int>(0);
-        public ReactiveProperty<float> Volume { get; private set; } = new ReactiveProperty<float>(1f); // ボリューム
-
+        public ReactiveProperty<float> Volume { get; private set; } = new ReactiveProperty<float>(1f);
 
         void Start()
         {
             _criAddressableAudioManager = CriAddressableAudioManager.Instance;
-            CurrentIndex.Subscribe(PlayMisic);
+
+            // 曲の再生とボリューム設定の反映
+            CurrentIndex.Subscribe(PlayMusic);
+            _volumeSlider.onValueChanged.AddListener(SetVolume);
+
+            // ボタンイベントの設定
             _nextButton.onClick.AddListener(Next);
-            _BackButton.onClick.AddListener(Back);
-            _MusicDropdown.ClearOptions();
-            _MusicDropdown.options.Add(new Dropdown.OptionData { text = "None" });
+            _backButton.onClick.AddListener(Back);
+
+            // Dropdownの初期設定
+            _musicDropdown.ClearOptions();
+            _musicDropdown.options.Add(new Dropdown.OptionData { text = "None" });
+            foreach (var audio in _audioList)
+            {
+                _musicDropdown.options.Add(new Dropdown.OptionData { text = audio.ToString() });
+            }
+
+            // Dropdownの選択変更時イベント
+            _musicDropdown.onValueChanged.AddListener(DropdownValueChanged);
         }
 
         /// <summary>
         /// インデックスで再生を変更
         /// </summary>
         /// <param name="playIndex"></param>
-        public async void PlayMisic(int playIndex)
+        public async void PlayMusic(int playIndex)
         {
+            if (playIndex < 0 || playIndex >= _audioList.Count) return;
+
+            _currentPlayback?.Stop();
+
             _currentPlayback = await _criAddressableAudioManager.StartPlayback(_audioList[playIndex]);
-            // Debug.Log(_audioList[playIndex].ToString());
+            _musicNameText.text = _audioList[playIndex].ToString();
         }
 
         /// <summary>
@@ -52,7 +66,10 @@ namespace HikanyanLaboratory
         /// </summary>
         public void Next()
         {
-            CurrentIndex.Value++;
+            if (CurrentIndex.Value < _audioList.Count - 1)
+            {
+                CurrentIndex.Value++;
+            }
         }
 
         /// <summary>
@@ -60,20 +77,25 @@ namespace HikanyanLaboratory
         /// </summary>
         public void Back()
         {
-            CurrentIndex.Value--;
+            if (CurrentIndex.Value > 0)
+            {
+                CurrentIndex.Value--;
+            }
         }
 
+        /// <summary>
+        /// ボリュームを設定
+        /// </summary>
+        /// <param name="volume"></param>
         public void SetVolume(float volume)
         {
-            _currentPlayback.SetVolume(volume);
+            _currentPlayback?.SetVolume(volume);
+            Volume.Value = volume;
         }
 
-        private void AudioSet()
+        private void DropdownValueChanged(int value)
         {
-            // foreach (Dropdown.OptionData audio in _audioList)
-            // {
-            //     _MusicDropdown.options.Add(audio);
-            // }
+            CurrentIndex.Value = value - 1;
         }
     }
 }
