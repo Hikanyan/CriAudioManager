1828eb78425ae0985e987e1f443a73d3b1b69660
Hikanyan <hikanyan0131@gmail.com>
2024-12-04 21:19:21 +0900

    とりあえず再生できた

--- Assets/HikanyanLaboratory/Script/AudioManager/Test.cs
+++ Assets/HikanyanLaboratory/Script/AudioManager/Test.cs
@@ -1,16 +1,20 @@
+using System.Threading.Tasks;
+using CriWare;
+using CriWare.Assets;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 
 namespace HikanyanLaboratory.Audio.Audio_Manager
 {
     public class Test : MonoBehaviour
     {
+        [SerializeField] CueReference cueReference;
+        private Task<CriAtomExPlayback> _currentPlayback;
+
         public void Start()
         {
-            AudioManager.Instance.Initialize();
             AudioManager.Instance.SetMasterVolume(0.8f);
-            AudioManager.Instance.PlayBGM("music_00");
-            AudioManager.Instance.PlaySE("gun1_High");
-            AudioManager.Instance.Enable3DSound(true);
+            _currentPlayback = AudioManager.Instance.Play(AudioManager.CriAudioType.BGM, cueReference);
         }
     }
 }
